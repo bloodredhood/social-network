e93a2ccc95e7af8fe9c0617889254d6044a524b1
bloodredhood <3drusher@gmail.com>
2021-12-12 18:40:02 +0300

    hide UserC into containr,rfctr User as fresh pureF

--- src/components/pages/users/Users.js
+++ src/components/pages/users/Users.js
@@ -1,26 +1,29 @@
-import * as axios from "axios";
 import React from "react";
+import styles from "./Users.module.css"
 import userPhoto from "../../../assets/images/user.png";
 
 const Users = (props) => {
 
-  const getUsers = () => {
-    if (props.users.length === 0) {
-      axios.get("/users").then(response => {
-        console.log(response);
-        props.setUsers(response.data)})
-    }
+  const pagesCount = Math.ceil(props.totalUsersCount / props.pageSize)
+
+  const pages = []
+  for (let i = 1; i <= pagesCount; i++) {
+    pages.push(i)
   }
 
   return (
     <div>
-      <button onClick={getUsers}>Get Users</button>
+      <div className={styles.continerForBreadCrumb}>
+        {pages.map( p => {
+          return <span className={props.currentPage === p && styles.selectedPage} onClick={(e) => {props.onPageChanged(p)}} >{p}</span>
+        })}
+      </div>
       {
         props.users.map(u =>
           <div key={u.id}>
             <span>
               <div>
-                <img style={{ width: "50px", height: "50px" }} src={ u.photo != null ? u.photo : userPhoto} alt="" />
+                <img style={{ width: "75px", height: "75px" }} src={u.photos.small != null ? u.photos.small : userPhoto} alt="" />
               </div>
               <div>
                 {u.followed
@@ -32,12 +35,8 @@ const Users = (props) => {
             </span>
             <span>
               <span>
-                <div>{u.fullName}</div>
+                <div>{u.name}</div>
                 <div>{u.status}</div>
-              </span>
-              <span>
-                <div>{u.location.country}</div>
-                <div>{u.location.city}</div>
               </span>
             </span>
           </div>
