d65f3087bc4db24ed155a2f78aaf5740e7c8aca6
bloodredhood <3drusher@gmail.com>
2021-12-24 14:31:01 +0300

    compDidUpdate at profile status

--- src/components/pages/profile/profile-items/ProfileStatus.js
+++ src/components/pages/profile/profile-items/ProfileStatus.js
@@ -25,6 +25,13 @@ class ProfileStatus extends React.Component {
     })
   }
 
+  componentDidUpdate(prevProps, prevState) {
+    if (prevProps.status !== this.props.status)
+    this.setState({
+      status: this.props.status
+    })
+  }
+
   render() {
     return (
       <div>
