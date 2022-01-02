538085a48ae1ba92272020a45456735409cb953e
bloodredhood <3drusher@gmail.com>
2022-01-02 17:17:27 +0300

    api and reducer is rdy for login

--- src/redux/authReducer.js
+++ src/redux/authReducer.js
@@ -2,7 +2,7 @@ import {authAPI} from "../api/api"
 
 const SET_AUTH_USER_DATA = "SET_AUTH_USER_DATA"
 
-export const setAuthUserData = (userId, email, login) => ({ type: SET_AUTH_USER_DATA, data: {userId, email, login} })
+export const setAuthUserData = (userId, email, login, isAuth) => ({ type: SET_AUTH_USER_DATA, payload: {userId, email, login, isAuth} })
 
 const initialState = {
   userId: null,
@@ -16,7 +16,7 @@ const authReducer = (state = initialState, action) => {
     case SET_AUTH_USER_DATA:
       return {
         ...state,
-        ...action.data,
+        ...action.payload,
 				isAuth: true
       }
     default: return state
@@ -27,7 +27,25 @@ export const getAuthUserData = () => dispatch => {
   authAPI.me().then(response => {
     if (response.data.resultCode === 0) {
       const {id, email, login} = response.data.data
-      dispatch(setAuthUserData(id, email, login))
+      dispatch(setAuthUserData(id, email, login, true))
+    }
+  })
+}
+
+export const login = (email, password, rememberMe) => dispatch => {
+  authAPI.login(email, password, rememberMe)
+  .then(response => {
+    if (response.data.resultCode === 0) {
+      dispatch(getAuthUserData())
+    }
+  })
+}
+
+export const logout = () => dispatch => {
+  authAPI.logout()
+  .then(response => {
+    if (response.data.resultCode === 0) {
+      dispatch(setAuthUserData(null, null, null, false))
     }
   })
 }
