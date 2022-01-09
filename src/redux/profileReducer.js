9051f20bf048d9346517381d1ac08e648ebf1be3
bloodredhood <3drusher@gmail.com>
2022-01-09 18:14:36 +0300

    profileReducer test

--- src/redux/profileReducer.js
+++ src/redux/profileReducer.js
@@ -3,10 +3,12 @@ import {profileAPI, usersAPI} from "../api/api"
 const ADD_POST = "ADD_POST"
 const SET_USER_PROFILE = "SET_USER_PROFILE"
 const SET_STATUS = "SET_STATUS"
+const DELETE_POST = "DELETE_POST"
 
 export const addPostActionCreator = (newPostText) => ({ type: ADD_POST, newPostText })
 export const setUserProfile = profile => ({type: SET_USER_PROFILE, profile})
 export const setStatus = status => ({type: SET_STATUS, status})
+export const deletePost = postId => ({type: DELETE_POST, postId})
 
 const initialState = {
   posts: [
@@ -41,6 +43,8 @@ const profileReducer = (state = initialState, action) => {
           status: action.status
       }
     }
+    case DELETE_POST:
+      return {...state, posts: state.posts.filter( p => p.id !== action.postId )}
     default: return state
   }
 }
