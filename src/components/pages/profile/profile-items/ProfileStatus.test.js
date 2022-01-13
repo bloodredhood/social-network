8a36bec0b65e3306ae2ec8df568625ae0cc3fb05
bloodredhood <3drusher@gmail.com>
2022-01-13 18:28:39 +0300

    two tests added to ProfileStatus.test

--- src/components/pages/profile/profile-items/ProfileStatus.test.js
+++ src/components/pages/profile/profile-items/ProfileStatus.test.js
@@ -13,9 +13,9 @@ describe("ProfileStatus component", () => {
   test("after creation input shouldn't be displayed", () => {
     const component = create(<ProfileStatus status="status first" />)
     const root = component.root
-    
+
     expect(() => {
-      let input = root.findByType("input")
+      root.findByType("input")
     }).toThrow()
   })
 
@@ -24,5 +24,22 @@ describe("ProfileStatus component", () => {
     const root = component.root
     let span = root.findByType("span")
     expect(span.children[0]).toBe('status first')
+  })
+
+  test("input should be displayed in editMode instead of span", () => {
+    const component = create(<ProfileStatus status="status first" />)
+    const root = component.root
+    let span = root.findByType("span")
+    span.props.onClick()
+    let input = root.findByType("input")
+    expect(input.props.value).toBe('status first')
+  })
+
+  test("callback should be called", () => {
+    const mockCallback = jest.fn()
+    const component = create(<ProfileStatus status="status first" updateStatus={mockCallback} />)
+    const instance = component.getInstance()
+    instance.deactivateEditMode()
+    expect(mockCallback.mock.calls.length).toBe(1)
   })
 })
