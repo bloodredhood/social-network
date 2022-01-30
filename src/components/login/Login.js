cdf1285967079bb8f52802a280c5c355dcb14c63
bloodredhood <3drusher@gmail.com>
2022-01-30 18:07:49 +0300

    login captcha is ready

--- src/components/login/Login.js
+++ src/components/login/Login.js
@@ -7,7 +7,7 @@ import { Navigate } from "react-router";
 const Login = (props) => {
 
   const onSubmit = (formData) => {
-    props.login(formData.email, formData.password, formData.rememberMe);
+    props.login(formData.email, formData.password, formData.rememberMe, formData.captcha);
   }
 
   if (props.isAuth) {
@@ -17,12 +17,13 @@ const Login = (props) => {
 	return (
 		<div>
 			<h1>Login</h1>
-      <LoginReduxForm onSubmit={onSubmit}/>
+      <LoginReduxForm onSubmit={onSubmit} captchaUrl={props.captchaUrl}/>
 		</div>
 	)
 }
 
 const mapStateToProps = state => ({
+  captchaUrl: state.auth.captchaUrl,
   isAuth: state.auth.isAuth
 })
 
--- src/components/login/LoginForm.js
+++ src/components/login/LoginForm.js
@@ -4,12 +4,17 @@ import { required } from "../../utils/validators/validators";
 import { createField, Input } from "../common/FormsControls/FormsControls";
 import "../common/FormsControls/FormsControls.module.css"
 
-const LoginForm = ({ handleSubmit, error }) => {
+const LoginForm = ({ handleSubmit, error, captchaUrl }) => {
   return (
     <form onSubmit={handleSubmit} >
       {createField("Email", "email", [required], Input)}
       {createField("Password", "password", [required], Input, { type: "password" })}
-      {createField(null, "rememberMe", [], Input, { type: "checkbox" }, "remember me")}
+      {createField(
+          null, "rememberMe", [], Input, { type: "checkbox" }, "remember me"
+      )}
+
+      {captchaUrl && <img alt="captcha" src={captchaUrl}/>}
+      {captchaUrl && createField("", "captcha", [required], Input, {})}
       {
         error && <div className="formSummaryError">
           {error}
